5c678c64b4c02a30c9764faaf05306ef80695d4d
Kyle Shipley <shipstar@gmail.com>
2015-12-12 14:06:57 -0500

    Now featuring separate LEDs for button press and room status

--- firmware.ino
+++ firmware.ino
@@ -1,43 +1,53 @@
 /* Pins configuration */
 int inPin = D3;  // button
-int outPin = D7; // led
-int outPin2 = D6; // led
+int buttonPressLED = D7; // led
+int roomStatusLED = D6; // led
 
 /* Button and led logics */
-int state = HIGH;     // current state of the output pin
-int reading;          // current reading from the input pin
-int previous = LOW;   // previous reading from the input pin
+bool roomOccupied = FALSE;
+int buttonPressLEDState = HIGH;     // current state of the output pin
+int currentButtonReading;          // current reading from the input pin
+int previousButtonReading = LOW;   // previous reading from the input pin
 long lastToggleTime = 0;           // the last time the output pin was toggled
 long debounce = 200;  // the debounce time, increase if the output flickers
 long ledDuration = 2000;  // duration LED should stay lit after press
 
 void setup() {
     pinMode(inPin, INPUT);    // button pin setup
-    pinMode(outPin, OUTPUT);  // led pin setup
-    pinMode(outPin2, OUTPUT);  // led pin setup
+    pinMode(buttonPressLED, OUTPUT);  // led pin setup
+    pinMode(roomStatusLED, OUTPUT);  // led pin setup
 }
 
 void loop() {
-    reading = digitalRead(inPin);  // read the button state
+    currentButtonReading = digitalRead(inPin);  // read the button state
 
-    if (state == HIGH && millis() - lastToggleTime > ledDuration) {
-        state = LOW;
-        digitalWrite(outPin, LOW);
-        digitalWrite(outPin2, LOW);
+    if (buttonPressLEDState == HIGH && millis() - lastToggleTime > ledDuration) {
+        buttonPressLEDState = LOW;
+        digitalWrite(buttonPressLED, LOW);
     }
+
     if (millis() - lastToggleTime > debounce) {
         // We have waited long enough to rule out this being random power fluctuation
-        if (reading == HIGH && previous == LOW) {
-            // If the input just went from LOW and HIGH and we've waited long enough to ignore
-            // any noise on the circuit, toggle the output pin and remember the time
-            Spark.publish("button_state", "pressed", 60, PRIVATE);
-            digitalWrite(outPin, HIGH);
-            digitalWrite(outPin2, HIGH);
-            state = HIGH;
-
-            lastToggleTime = millis();
+        if (currentButtonReading == HIGH && previousButtonReading == LOW) {
+            handleButtonPress();
         }
     }
 
-    previous = reading;
+    previousButtonReading = currentButtonReading;
+}
+
+void handleButtonPress() {
+    if (roomOccupied) {
+        Spark.publish("room_occupied", "NOT_OCCUPIED", 60, PRIVATE);
+        digitalWrite(roomStatusLED, LOW);
+        roomOccupied = FALSE;
+    } else {
+        Spark.publish("room_occupied", "OCCUPIED", 60, PRIVATE);
+        digitalWrite(roomStatusLED, HIGH);
+        roomOccupied = TRUE;
+    }
+
+    digitalWrite(buttonPressLED, HIGH);
+    buttonPressLEDState = HIGH;
+    lastToggleTime = millis();
 }
