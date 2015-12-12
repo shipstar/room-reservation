c1abd599b2c917aff2613e60f55abf29a7d1011d
Kyle Shipley <shipstar@gmail.com>
2015-12-12 11:44:22 -0500

    Switch to ES6 style.

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 var spark = require('spark')
 
-spark.on('login', function() {
-  spark.getEventStream(false, 'mine', function(data) {
+spark.on('login', () => {
+  spark.getEventStream(false, 'mine', (data) => {
     console.log("Event: ", JSON.stringify(data))
   })
 })
+
+// spark.on('login', function() {
+//   spark.getEventStream(false, 'mine', function(data) {
+//     console.log("Event: ", JSON.stringify(data))
+//   })
+// })
 
 spark.login({ accessToken: process.env.SPARK_TOKEN })
